4c6e384cdd1fe30705b9bb4d8aa9292a024e0409
sachin-me <sachinkumartiwari363@gmail.com>
2020-11-18 15:40:35 +0530

    add build

---
+++ rgolsgym/build/precache-manifest.747a06ad49ebf49d017cfebf5ddc0f74.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800703eba5c2725a14560703c431d2b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6c3a17ef451bc62d324",
+    "url": "/static/css/main.9adb4e1c.chunk.css"
+  },
+  {
+    "revision": "38f76d458f6958a075c2",
+    "url": "/static/js/2.9682695c.chunk.js"
+  },
+  {
+    "revision": "657d7bea15aac7ff6194a28a57e164c7",
+    "url": "/static/js/2.9682695c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c3a17ef451bc62d324",
+    "url": "/static/js/main.6cdfaec1.chunk.js"
+  },
+  {
+    "revision": "e1c434c4ca0161ed5a98",
+    "url": "/static/js/runtime-main.403965eb.js"
+  },
+  {
+    "revision": "7ca489401c4d42e3c3d198d62f97635c",
+    "url": "/static/media/about-us.7ca48940.jpg"
+  },
+  {
+    "revision": "8698848f5a24b90624d91c33f1aee55d",
+    "url": "/static/media/class-1.8698848f.jpg"
+  },
+  {
+    "revision": "288f9dc3a79cad06a0546bb3040f4f81",
+    "url": "/static/media/class-2.288f9dc3.jpg"
+  },
+  {
+    "revision": "e43d51384c2aea39bf125a99cd847f6b",
+    "url": "/static/media/class-3.e43d5138.jpg"
+  },
+  {
+    "revision": "4caa7382a350a6dc5c7fa7608d1d94f3",
+    "url": "/static/media/class-4.4caa7382.jpg"
+  },
+  {
+    "revision": "3f0a3b4348e2f9c672b8c93fa3491bd1",
+    "url": "/static/media/class-5.3f0a3b43.jpg"
+  },
+  {
+    "revision": "ba3a4119bb6a098e19df7028d6f8ec28",
+    "url": "/static/media/class-6.ba3a4119.jpg"
+  },
+  {
+    "revision": "fe94c77ee3e9141c8fd48228eb81f3e9",
+    "url": "/static/media/congratulation.fe94c77e.png"
+  },
+  {
+    "revision": "6d78ce2d2724c688f0f0edd923829032",
+    "url": "/static/media/header_background.6d78ce2d.jpg"
+  },
+  {
+    "revision": "3f86cb49ef8dcc72787764681eda4f32",
+    "url": "/static/media/option-1.3f86cb49.jpg"
+  },
+  {
+    "revision": "c5bf4858422525ec051770972af32a45",
+    "url": "/static/media/option-2.c5bf4858.jpg"
+  },
+  {
+    "revision": "79ef02a1f01c5a0435d7cf1b50442a65",
+    "url": "/static/media/option-3.79ef02a1.jpg"
+  },
+  {
+    "revision": "c42e92ba02cae85f2fdd51234d4256f6",
+    "url": "/static/media/payPal.c42e92ba.png"
+  },
+  {
+    "revision": "e3192ed31c1ca125825b7e80d2d617a4",
+    "url": "/static/media/payment1.e3192ed3.png"
+  },
+  {
+    "revision": "d40af3bd364fadbf7bfe2d785fea218c",
+    "url": "/static/media/payment2.d40af3bd.png"
+  },
+  {
+    "revision": "b2614adc7f8562b3f644cd932221d03b",
+    "url": "/static/media/program-1.b2614adc.png"
+  },
+  {
+    "revision": "ef035eec74f5ff974519b6267d7f5a85",
+    "url": "/static/media/program-2.ef035eec.jpg"
+  }
+]);
